9c474849d041b243d14e453e709c3ca64360eb46
Muhammet <nsaryyev9797@gmail.com>
2021-06-25 20:43:06 +0300

    1_creational => 3_prototype pattern

---
+++ 1_creational/2_factory.js
@@ -0,0 +1,70 @@
+class SimpleMembership {
+
+    constructor(name) {
+        this.name = name;
+        this.cost = 50;
+    }
+
+}
+
+class StandartMembership {
+
+    constructor(name) {
+        this.name = name;
+        this.cost = 150;
+    }
+
+}
+
+class PremiumMembership {
+
+    constructor(name) {
+        this.name = name;
+        this.cost = 500;
+    }
+
+}
+
+class MemberFactory {
+
+    static list = {
+        simple: SimpleMembership,
+        standart: StandartMembership,
+        premium: PremiumMembership
+    }
+
+    create(name, type = 'simple') {
+
+        const Membership = MemberFactory.list[type];
+
+        const member = new Membership(name);
+
+        member.type = type;
+
+        member.define = function () {
+
+            console.log(`${this.name} (${this.type}): ${this.cost} `);
+
+        }
+
+        return member;
+
+    }
+
+}
+
+const factory = new MemberFactory();
+
+const members = [
+
+    factory.create('Vladilen', 'simple'),
+
+    factory.create('Elena', 'premium'),
+
+    factory.create('Vasilisa', 'standart')
+
+]
+
+members.forEach(m=>{
+    m.define()
+})
